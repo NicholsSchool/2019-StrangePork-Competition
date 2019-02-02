7b3ffc1f2ac461703d1a4d2f11e8914842f2ef53
James-coburn <CoburnJamesE@gmail.com>
2019-02-02 15:44:28 -0500

    hatch scoop

---
+++ src/main/java/frc/robot/commands/getHatch.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class getHatch extends Command
+{
+    double speed;
+    double time;
+
+    public getHatch(double sp, double tm)
+    {
+        speed = sp;
+        time = tm;
+    }
+
+    
+}
