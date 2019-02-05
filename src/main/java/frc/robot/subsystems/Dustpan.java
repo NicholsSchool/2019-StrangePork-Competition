9e743f2ff4a8c4163d93afcb7224ec9a1418cf94
Jack <jack@Jacks-MBP-3.nicholsschool.org>
2019-02-05 17:26:40 -0500

    Created dustpan commands and subsystem

--- src/main/java/frc/robot/subsystems/Dustpan.java
+++ src/main/java/frc/robot/subsystems/Dustpan.java
@@ -1,8 +1,24 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Constants;
+import frc.robot.RobotMap;
 
-public class Dustpan extends Subsystem
+public class DustPan
 {
-    
-}
+    public void raise()
+    {
+        RobotMap.solenoid0.set(Constants.DUSTPAN_RAISED);
+    }
+    public void place()
+    {
+
+    }
+    public void drop()
+    {
+        RobotMap.solenoid0.set(Constants.DUSTPAN_DROPPED);
+    }
+    public void stop()
+    {
+
+    }
+}                                        
