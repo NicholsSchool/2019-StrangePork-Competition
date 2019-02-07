7b2d12f88c5f4aaf62ed91b28c1239af3eaefe78
James-coburn <CoburnJamesE@gmail.com>
2019-02-07 18:10:10 -0500

    AngleTurn Comments

--- src/main/java/frc/robot/commands/AngleTurn.java
+++ src/main/java/frc/robot/commands/AngleTurn.java
@@ -8,7 +8,13 @@ public class AngleTurn extends Command
 {
     public double speed;
     public double desiredAngle;
-   
+    /**
+     * The AngleTurn class turns the robot to a desired angle 
+     * using the NavX sensor, using the DriveTrain.
+     * 
+     * @param agl - the angle the robot will rotate to
+     * @param spd - the speed which the robot will rotate
+     */
     public AngleTurn(double agl, double spd)
     {
         speed = spd;
@@ -16,25 +22,35 @@ public class AngleTurn extends Command
 
         requires(Robot.driveTrain);
     }
+
     @Override
     public void initialize()
     {
         Robot.navX.reset();
     }
 
+    /**
+     * The excecute method determines wether the robot turns
+     * to the right or left, depending on which direction 
+     * is shorter.
+     */
     @Override
     protected void execute() 
     {
         if(desiredAngle > 0)
-        {
-        Robot.driveTrain.move(speed, -speed);    
-        }
-        else 
-        {
-        Robot.driveTrain.move(-speed, speed);
-        }
+            Robot.driveTrain.sigmoidMove(speed, -speed);    
+        
+        else
+            Robot.driveTrain.sigmoidMove(-speed, speed);
+        
     }
 
+    /**
+     * This method specifies a range of angles. the DriveTrain
+     * stops when the robot is at an angle within the range.
+     * The range is necessary becouse otherwise the robot would never 
+     * return an angle that is precisely the value the method requires.   
+     */
     @Override
     protected boolean isFinished()
     {
